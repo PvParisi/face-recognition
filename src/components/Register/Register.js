fb7709c982e41e79b141505daffe952990f3af39
Piervincenzo Parisi <piervi_parisi@yahoo.it>
2018-03-18 18:05:28 +0100

    added rank feature

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -33,14 +33,14 @@ class Register extends React.Component {
                 name: this.state.name
             })
         })
-            .then(response => response.json())
-            .then(user => {
-                if (user) {
-                    this.props.loadUser(user);
-                    this.props.onRouteChange('home');
-                }
-            })
-            .catch(console.log);
+        .then(response => response.json())
+        .then(user => {
+            if (user) {
+                this.props.loadUser(user);
+                this.props.onRouteChange('home');
+            }
+        })
+        .catch(console.log);
     }
 
     render() {
